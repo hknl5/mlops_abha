ee47170c03400f9b80a316b0151e8ff60286e936
hknl5 <hanouf.qh2004@gmail.com>
2025-07-15 12:38:34 +0300

    Added multiplication function to main.py

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def sub(a, b):
 def add(a, b):
     """Add two numbers."""
     return a + b
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
 
 if __name__ == "__main__":
     result = add(3, 5)
